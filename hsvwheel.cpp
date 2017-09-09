f4caee940e70d7e4e343090dae0773bbfff085b3
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2017-09-09 14:19:51 +0200

    hexa

--- hsvwheel.cpp
+++ hsvwheel.cpp
@@ -48,13 +48,57 @@ void HSVtoRGB( double & r, double & g, double & b,
   }
 }
 
+
+template<typename Board>
+void addHexa(const int i, const int j,  Board &board)
+{
+  std::vector<LibBoard::Point> poly;
+  double r= 0.55;// 0.5*cos(M_PI/6.0);
+  double di = 0.5*j + i;
+  double dj = j*sqrt(3.0)/2.0;
+  for(int ii = 0; ii < 6; ++ii)
+  {
+    double angle = 60*ii+30;
+    poly.push_back( LibBoard::Point(di+r*cos( angle * M_PI/180.0),  dj+r*sin(angle * M_PI/180.0)));
+  }
+  double rr,gg,bb;
+  double h = 360.0/(2.0*M_PI)*(M_PI + atan2(dj+0.001,di+0.001));
+  double s= std::sqrt((di*di+dj*dj))/ 10.0;
+  // s = 0.8;
+ // if (s<=1.0)
+  if (( ( abs(i)
+         + abs( i + j)
+         + abs(j)) / 2.0)< 10.0)
+  {
+    HSVtoRGB(	rr, gg, bb, h, s, 1.0);
+    Color col(static_cast<int>( rr * 255),
+              static_cast<int>( gg * 255),
+              static_cast<int>( bb * 255));
+    board.setPenColor( col );
+    board.fillPolyline(poly);
+  }
+  
+}
+
+
 int main()
 {
   Domain dom(Point(-100,-100), Point(100,100));
   
   const double eps=0.001;
   Board2D board;
-  for(auto p : dom )
+  
+  double r,g,b;
+  for(auto i=-40; i<51; ++i)
+    for(auto j=-40; j < 51; ++j)
+    {
+     
+      addHexa(i,j,board);
+      
+    }
+  
+  
+  /*for(auto p : dom )
   {
     double r,g,b;
     double h = 360.0/(2.0*M_PI)*(M_PI + atan2(p[1]+eps,p[0]+eps));
@@ -69,7 +113,7 @@ int main()
     board.setFillColor(col);
     board << CustomStyle( p.className(), new CustomColors( col, col ) ) << p;
     
-  }
+  }*/
   
   board.saveCairo("hsvwheel.pdf", Board2D::CairoPDF);
   return 0;
